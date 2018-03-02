89bcb3f480cf49b27ae906fbbd35db08211f5a23
blha303 <stevensmith.ome@gmail.com>
2018-03-03 05:29:33 +0800

    Added peerflix support

--- README.md
+++ README.md
@@ -22,6 +22,7 @@ $ rahr big buck bunny
 1: Big.Buck.Bunny.720p (Movies/Legal)
 Pick: (type 1)
 magnet:?xt=urn:btih:1234567890&dn=Big.Buck.Bunny.720p&tr=http%3A%2F%2Ftracker.trackerfix.com%3A80%2Fannounce
+$ rahr --help
 ```
 
 I'm not responsible for how you decide to use this program. I made it as an exercise only.
--- rahr/rahr.py
+++ rahr/rahr.py
@@ -2,6 +2,8 @@
 from requests import get
 from argparse import ArgumentParser
 import sys
+from subprocess import Popen
+from distutils.spawn import find_executable
 
 token = get("https://torrentapi.org/pubapi_v2.php?get_token=get_token").json()["token"]
 
@@ -9,23 +11,31 @@ def main():
     parser = ArgumentParser()
     parser.add_argument("search", nargs="+")
     parser.add_argument("--choice", type=int)
+    parser.add_argument("--peerflix", help="If present, calls peerflix with the selected magnet link", action="store_true")
     args = parser.parse_args()
     results = get("https://torrentapi.org/pubapi_v2.php", params={"mode": "search", "search_string": " ".join(args.search), "token": token, "format": "json"}).json()["torrent_results"]
     if results:
         for n,r in enumerate(results):
             print("{}: {} ({})".format(n, r["filename"], r["category"]), file=sys.stderr)
-        try:
-            if args.choice is not None:
-                choice = results[args.choice]
-            else:
-                choice = results[int(input("Pick: "))]
-        except IndexError:
-            print("Invalid choice", file=sys.stderr)
-            return 2
-        except ValueError:
-            print("Not a number", file=sys.stderr)
-            return 3
-        print(r["download"], end="")
+        if len(results) > 1:
+            try:
+                if args.choice is not None:
+                    choice = results[args.choice]
+                else:
+                    choice = results[int(input("Pick: "))]
+            except IndexError:
+                print("Invalid choice", file=sys.stderr)
+                return 2
+            except ValueError:
+                print("Not a number", file=sys.stderr)
+                return 3
+        else:
+            choice = results[0]
+        if args.peerflix:
+            process = Popen([find_executable("peerflix"), choice["download"]])
+            process.wait()
+        else:
+            print(choice["download"], end="")
         return 0
     else:
         print("No results", file=sys.stderr)
